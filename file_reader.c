faa010cee55a3d334e8085bc696a1b7ca8612903
Yunys26 <31537764+Yunys26@users.noreply.github.com>
2018-01-09 23:05:04 +0300

    Update file_reader.c

--- file_reader.c
+++ file_reader.c
@@ -1,37 +1,30 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include<string.h>
+#include <stdlib.h>
 
-int main() {
-	FILE *file;
-	char file_name[30];
-	scanf("%s", &file_name);
-	file = fopen(file_name, "r");
-	if (file == NULL) {
-		while (file == NULL){
-			printf("%s", "FILE NOT FOUND. Please, try again\n");
-			scanf("%s", &file_name);
-			file = fopen(file_name, "r");
-		}
-	}
-	
-	char text[1000];
-	int i = 0;
-	char *real_tail;
-
-	while (fgets(text, sizeof(text), file)){
-		real_tail = "";
-
-
-		if (text[strlen(text) - 1] == '\n')
-		{
-			real_tail = "\\n";
-			text[strlen(text) - 1] = '\0';
-		};	
-		printf("%s\n", text);
-	}
-	int size = ftell(file);
-	printf("File size = %d bytes\n", size);
-	fclose(file);
-	return 0;
+int main(int argc, char ** argv)
+{
+    char string[255], *ps = NULL;
+    FILE *f;
+    int sizze = 0, pos = 0;
+    f = fopen(argv[1], "r");
+    if (f == NULL)
+    {
+        while (f == NULL) {
+            printf("Please enter the correct file name\n");
+            scanf("%s\n", argv[1]);
+        }
+    }
+    while (fgets(string, sizeof(string), f))
+    {
+            ps = fgets(string, sizeof(string), f);
+            pos++;
+            printf("%s\n", ps);
+    }
+    rewind(f);
+    while ((fgetc(f)) != EOF)
+        sizze++;
+    fclose(f);
+    printf("Written %d records, total %d bytes\n", pos, sizze);
+    return 0;
 }
+
